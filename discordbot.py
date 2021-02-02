362bab6829604d66a11b8c2fa6c87008fce27630
Dtomper <74678283+dtomper@users.noreply.github.com>
2021-02-02 18:04:02 +0100

    Add files via upload

---
+++ discordbot.py
@@ -0,0 +1,31 @@
+import discord
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get('https://www.cleverbot.com')
+driver.find_element_by_id('noteb').click()
+
+
+def get_response(message):
+    driver.find_element_by_xpath('//*[@id="avatarform"]/input[1]').send_keys(message + Keys.RETURN)
+    while True:
+        try:
+            driver.find_element_by_xpath('//*[@id="snipTextIcon"]')
+            break
+        except:
+            continue
+    response = driver.find_element_by_xpath('//*[@id="line1"]/span[1]').text
+    return response
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        if message.author != self.user:
+            reponse = get_response(message.content)
+            await message.channel.send(f"{message.author.mention} {reponse}")
+
+client = MyClient()
+client.run('BOT TOKEN HERE')
